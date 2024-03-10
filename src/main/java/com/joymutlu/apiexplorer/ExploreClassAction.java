40f613a41f9ff2c1ac8e12a16312dc2f8dd0905a
Александр Плохой <alexplohoy@mail.ru>
2024-03-10 10:13:23 +0300

    fix description

--- src/main/java/com/joymutlu/apiexplorer/ExploreClassAction.java
+++ src/main/java/com/joymutlu/apiexplorer/ExploreClassAction.java
@@ -93,7 +93,6 @@ public class ExploreClassAction extends AnAction {
 }
 
 // TODO: 09.03.2024 fix own package scan
-// TODO: 09.03.2024 fix description (press only existing var)
 // TODO: 01.03.2024 Generate API for method call
 // TODO: 27.02.2024 Generate API tree(for any depth without recursion)
 
